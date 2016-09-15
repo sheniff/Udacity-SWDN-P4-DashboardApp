632a2a3a5a34ded099c01dd5a942eb5639350706
Sergio Almecija <sheniff@gmail.com>
2016-09-15 10:06:23 -0700

    show data for csv table. Sorting pending.

--- src/app/components/Data.ts
+++ src/app/components/Data.ts
@@ -1,4 +1,24 @@
-class DataController {}
+import { csvRawData } from '../services/payloads';
+
+const FIELDS = ['submissionDate', 'customerName', 'customerEmail',
+'description', 'openClosed', 'closedDate', 'employeeName'];
+
+class DataController {
+  public data: Array<{}> = [];
+
+  $onInit() {
+    this.data = this.parseData(csvRawData);
+  }
+
+  parseData(raw: string): Array<{}> {
+    return raw.split('\n').map(function(row){
+      return row.split(',').reduce(function(res, cell, index){
+        res[FIELDS[index]] = cell;
+        return res;
+      }, {});
+    });
+  }
+}
 
 export const Data: angular.IComponentOptions = {
   template: require('./Data.html'),
--- src/app/services/payloads.ts
+++ src/app/services/payloads.ts
@@ -25,3 +25,32 @@ export function generateRandomTimeline(days: number): Array<ITimeData> {
 
   return data;
 };
+
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+function generateRandomDates(numDates) {
+  var dates = [];
+  for (var i = 0; i < 15; i++) {
+    dates.push(
+      randomDate(i === 0 ?
+          new Date(2010, 0, 1) :
+          new Date(dates[i-1]),
+        new Date()
+      )
+    );
+  }
+  return dates;
+}
+
+export const csvRawData = '1397118824518,Nike,issues@nike.com,Cannot access my data,open,,Mark\n\
+1399607428366,Adidas,issues@adidas.com,Something weird happens,closed,1399866628366,John\n\
+1467106945001,Nike,issues@nike.com,API access broken,closed,1467970945001,Mark\n\
+1467546348630,Levis,issues@levis.com,Something does not work,open,,Mark\n\
+1473361284544,Adidas,issues@adidas.com,Offers invalid in campaign,closed,1473534084544,John\n\
+1473550488863,Nike,issues@nike.com,Thanks for your help,open,,John\n\
+1473924727470,Levis,issues@levis.com,Please help!,closed,1474961527470,Mark\n\
+1473955813281,Adidas,issues@adidas.com,I dont know what is going on,open,,Lewis\n\
+1473956502014,Levis,issues@levis.com,My offers are not visible to public,open,,Mark\n\
+1473956816902,Levis,issues@levis.com,Please help please!,closed,1474561616902,Lewis';
